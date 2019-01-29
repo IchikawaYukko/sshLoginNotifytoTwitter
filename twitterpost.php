6de75dcb640f56ee118d44f2f507afaf1e5debc9
IchikawaYukko <ichikawayuriko@yahoo.co.jp>
2019-01-30 05:55:41 +0900

    fix uptime source. read /proc instead of calling uptime.

--- twitterpost.php
+++ twitterpost.php
@@ -70,7 +70,14 @@ function GeoIP($ipaddr) {
 }
 
 function getUptime() {
-  return exec ("uptime |cut -f 4-7 -d ' '");
+  $str   = file_get_contents('/proc/uptime');
+  $num   = floatval($str);
+  $secs  = fmod($num, 60); $num = (int)($num / 60);
+  $mins  = $num % 60;      $num = (int)($num / 60);
+  $hours = $num % 24;      $num = (int)($num / 24);
+  $days  = $num;
+
+  return "$days days";
 }
 
 function getLoginBonus() {
@@ -82,11 +89,9 @@ function getLoginBonus() {
 
   foreach($bonusfile as $line) {
     if($line === false) continue;
-    //array_push( $bonus, $loginBonusMessage.$line );
     $bonus[] = $loginBonusMessage.$line;
   }
   $bonus[] = $uptimeMessage;
-  //array_push( $bonus, $uptimeMessage );
 
   shuffle( $bonus );
   return $bonus[0];
